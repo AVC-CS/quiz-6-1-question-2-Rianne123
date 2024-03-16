e1d94284db5fc0127c345d615b605a96993ef873
Rian-stack <lpapa@students.avc.edu>
2024-03-15 23:11:33 -0700

    added structure for write and read file functions

--- main.hpp
+++ main.hpp
@@ -1,3 +1,21 @@
-/***************************************************
- * Code your program here
- ***************************************************/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std; 
+
+int writeFile() {
+    ifstream inFile(filename);
+    if (!inFile) {
+        cerr << "Error opening file." << endl;
+        return 0;
+    }
+}
+
+int readFile() {
+    ifstream inFile(filename);
+    if (!inFile) {
+        cerr << "Error opening file." << endl;
+        return 0;
+    }
+}
